f94a5689ee3392a143a8495258d2aa32d3bc432e
cyara <cmyarap@gmail.com>
2023-12-01 00:32:02 -0500

    Se agregan ajustes

--- app.py
+++ app.py
@@ -1,44 +1,25 @@
-from app import createApp, db
+from app import createApp, db, Routes
 
 app1 = createApp('development')
 
-from app.Routes.UsersRoutes import userMain
-from app.Routes.authRoutes import authMain
-from app.Routes.TicketsRoutes import ticketsMain
-from app.Routes.DistributorRoutes import distributorsMain
-from app.Routes.HostingRoutes import hostingsMain
-from app.Routes.DomainRoutes import domainsMain
-from app.Routes.BuyoutRoutes import buyoutsMain
-from app.Routes.CategoryRoutes import categoriesMain
-from app.Routes.CountryRoutes import countriesMain
-from app.Routes.PayModeRoutes import payModesMain
-from app.Routes.PayPlanRoutes import payPlansMain
-from app.Routes.PlanRoutes import plansMain
-from app.Routes.PlatformRoutes import platformsMain
-from app.Routes.RolRoutes import rolsMain
-from app.Routes.CreditCardRoute import creditCardsMain
-
-app1.register_blueprint(userMain)
-app1.register_blueprint(authMain)
-app1.register_blueprint(ticketsMain)
-app1.register_blueprint(distributorsMain)
-app1.register_blueprint(hostingsMain)
-app1.register_blueprint(domainsMain)
-app1.register_blueprint(buyoutsMain)
-app1.register_blueprint(categoriesMain)
-app1.register_blueprint(countriesMain)
-app1.register_blueprint(payModesMain)
-app1.register_blueprint(payPlansMain)
-app1.register_blueprint(plansMain)
-app1.register_blueprint(platformsMain)
-app1.register_blueprint(rolsMain)
-app1.register_blueprint(creditCardsMain)
+app1.register_blueprint(Routes.UsersRoutes.userMain)
+app1.register_blueprint(Routes.authRoutes.authMain)
+app1.register_blueprint(Routes.TicketsRoutes.ticketsMain)
+app1.register_blueprint(Routes.DistributorRoutes.distributorsMain)
+app1.register_blueprint(Routes.HostingRoutes.hostingsMain)
+app1.register_blueprint(Routes.DomainRoutes.domainsMain)
+app1.register_blueprint(Routes.BuyoutRoutes.buyoutsMain)
+app1.register_blueprint(Routes.CategoryRoutes.categoriesMain)
+app1.register_blueprint(Routes.CountryRoutes.countriesMain)
+app1.register_blueprint(Routes.PayModeRoutes.payModesMain)
+app1.register_blueprint(Routes.PayPlanRoutes.payPlansMain)
+app1.register_blueprint(Routes.PlanRoutes.plansMain)
+app1.register_blueprint(Routes.PlatformRoutes.platformsMain)
+app1.register_blueprint(Routes.RolRoutes.rolsMain)
+app1.register_blueprint(Routes.CreditCardRoute.creditCardsMain)
 
 if __name__ == '__main__':
     with app1.app_context():
         db.create_all() 
 
     app1.run()
-
-
-
