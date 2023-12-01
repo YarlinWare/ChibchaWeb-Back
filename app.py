8446404eb240675353ed1e366480187b8db4c336
cyara <cmyarap@gmail.com>
2023-12-01 00:26:27 -0500

    Se agregan ajustes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app import createApp, db
 
-app = createApp('development')
+app1 = createApp('development')
 
 from app.Routes.UsersRoutes import userMain
 from app.Routes.authRoutes import authMain
@@ -18,27 +18,27 @@ from app.Routes.PlatformRoutes import platformsMain
 from app.Routes.RolRoutes import rolsMain
 from app.Routes.CreditCardRoute import creditCardsMain
 
-app.register_blueprint(userMain)
-app.register_blueprint(authMain)
-app.register_blueprint(ticketsMain)
-app.register_blueprint(distributorsMain)
-app.register_blueprint(hostingsMain)
-app.register_blueprint(domainsMain)
-app.register_blueprint(buyoutsMain)
-app.register_blueprint(categoriesMain)
-app.register_blueprint(countriesMain)
-app.register_blueprint(payModesMain)
-app.register_blueprint(payPlansMain)
-app.register_blueprint(plansMain)
-app.register_blueprint(platformsMain)
-app.register_blueprint(rolsMain)
-app.register_blueprint(creditCardsMain)
+app1.register_blueprint(userMain)
+app1.register_blueprint(authMain)
+app1.register_blueprint(ticketsMain)
+app1.register_blueprint(distributorsMain)
+app1.register_blueprint(hostingsMain)
+app1.register_blueprint(domainsMain)
+app1.register_blueprint(buyoutsMain)
+app1.register_blueprint(categoriesMain)
+app1.register_blueprint(countriesMain)
+app1.register_blueprint(payModesMain)
+app1.register_blueprint(payPlansMain)
+app1.register_blueprint(plansMain)
+app1.register_blueprint(platformsMain)
+app1.register_blueprint(rolsMain)
+app1.register_blueprint(creditCardsMain)
 
 if __name__ == '__main__':
-    with app.app_context():
+    with app1.app_context():
         db.create_all() 
 
-    app.run()
+    app1.run()
 
 
 
